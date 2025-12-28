5f05cbdc4c5dae2e84c0483937e4a41adb5cc0a2
dsun14 <dashabalatsun@gmail.com>
2025-12-28 21:41:21 +0300

    Add UIColor and Anchor Extensions

---
+++ Instagram/Utils/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  Instagram
+//
+//  Created by Дарья Балацун on 28.12.25.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return  UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
