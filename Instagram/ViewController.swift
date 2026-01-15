acdedae9bbd7393d0e37161dfd569f505cb0d876
DashaBalatsun <dasha.balatsun@yandex.by>
2026-01-15 10:36:16 +0300

    Add profile photo upload functionality

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -6,8 +6,10 @@
 //
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let plusPhotoButton: UIButton = {
         let button = UIButton ()
@@ -21,9 +23,71 @@ class ViewController: UIViewController {
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
-        tf.addTarget(self, action: #selector(handleTextUnputChange), for: .editingChanged)
         return tf
     }()
+    
+    let usernameTextField: UITextField = {
+        let tf = UITextField ()
+        tf.placeholder = "Username"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return  tf
+    }()
+    
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sigh up", for: .normal)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+        button.isEnabled = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupViews()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    @objc func hundlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editingImage = info[.editedImage] as? UIImage {
+            plusPhotoButton.setImage(editingImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        plusPhotoButton.layer.borderWidth = 1
+        
+        dismiss(animated: true, completion: nil)
+    }
     
     @objc func handleTextUnputChange() {
         let isFormValid = emailTextField.text?.count ?? 0 > 0 && usernameTextField.text?.count ?? 0 > 0 && passwordTextField.text?.count ?? 0 > 0
@@ -36,81 +100,90 @@ class ViewController: UIViewController {
         }
     }
     
-    let usernameTextField: UITextField = {
-        let tf = UITextField ()
-        tf.placeholder = "Username"
-        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
-        tf.borderStyle = .roundedRect
-        tf.font = UIFont.systemFont(ofSize: 14)
-        tf.addTarget(self, action: #selector(handleTextUnputChange), for: .editingChanged)
-        return tf
-    }()
-    
-    let  passwordTextField: UITextField = {
-        let tf = UITextField()
-        tf.placeholder = "Password"
-        tf.isSecureTextEntry = true
-        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
-        tf.borderStyle = .roundedRect
-        tf.font = UIFont.systemFont(ofSize: 14)
-        tf.addTarget(self, action: #selector(handleTextUnputChange), for: .editingChanged)
-        return  tf
-    }()
-    
-    let signUpButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Sigh up", for: .normal)
-        button.layer.cornerRadius = 5
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
-        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
-        button.isEnabled = false
-        return button
-    }()
-    
     @objc func handleSignUp() {
         guard let email = emailTextField.text, email.count > 0 else { return }
-        guard let userName = usernameTextField.text, userName.count > 0 else { return }
+        guard let username = usernameTextField.text, username.count > 0 else { return }
         guard let password = passwordTextField.text, password.count > 0  else { return }
         
-        Auth.auth().createUser(withEmail: email, password: password) { user, error in
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             if let error = error {
-                print("Failed to create user", error)
+                print("Failed to create user", error.localizedDescription)
                 return
             }
-            print("Successfully crested user", user ?? "")
+            
+            guard let image = self.plusPhotoButton.imageView?.image else { return }
+            guard let uploadData = image.jpegData(compressionQuality: 0.3) else { return }
+            
+            let metadata = StorageMetadata()
+            metadata.contentType = "image/jpeg"
+            
+            let storageRef = Storage.storage().reference().child("profile_images")
+            let filename = NSUUID().uuidString
+            let profileImageRef = storageRef.child("\(filename).jpg")
+            
+            profileImageRef.putData(uploadData, metadata: metadata) { [weak self] metadata, error in
+                if let error = error {
+                    print("Failed to upload profile image", error.localizedDescription)
+                    return
+                }
+                
+                profileImageRef.downloadURL { url, error in
+                    if let error = error {
+                        print("Failed to get download URL:", error.localizedDescription)
+                        return
+                    }
+                    
+                    guard let profileImageUrl = url?.absoluteString else { return }
+                    print("Successfully uploaded profile image:", profileImageUrl)
+                    
+                    guard let user = authResult?.user else { return }
+                    print("Successfully crested user", user.uid)
+                    
+                    let dictinaryValues = ["username": username, "profileImageUrl": profileImageUrl]
+                    let values = [user.uid:dictinaryValues]
+                    Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { error, reference in
+                        if let error = error {
+                            print("Failed to save user into db", error.localizedDescription)
+                            return
+                        }
+                        print("Succesfully saved user info to db")
+                    })
+                }
+            }
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        view.addSubview(plusPhotoButton)
-        
-        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
-        plusPhotoButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 60, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 40, height: 140)
-        
+}
+
+
+extension ViewController {
+    func setupViews() {
+        setupPlusButton()
         setupInputFields()
-        
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        
-    }
-    
-    fileprivate func setupInputFields() {
+    func setupInputFields() {
         let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signUpButton])
-        
         stackView.distribution = .fillEqually
         stackView.axis = .vertical
         stackView.spacing = 10
         
         view.addSubview(stackView)
+        
+        emailTextField.addTarget(self, action: #selector(handleTextUnputChange), for: .editingChanged)
+        usernameTextField.addTarget(self, action: #selector(handleTextUnputChange), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(handleTextUnputChange), for: .editingChanged)
+        signUpButton.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
     
         stackView.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 20, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 200)
+    }
+    
+    func setupPlusButton() {
+        view.addSubview(plusPhotoButton)
+        plusPhotoButton.addTarget(self, action: #selector(hundlePlusPhoto), for: .touchUpInside)
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        plusPhotoButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 160, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 140, height: 140)
     }
 }
 
